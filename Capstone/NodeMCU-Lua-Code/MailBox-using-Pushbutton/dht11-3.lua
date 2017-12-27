07f7d8b08d2bfc428bf476fac57db1ed14f9c56b
Krish Mehta <mehtakrish95@gmail.com>
2017-12-27 00:56:30 -0800

    Update dht11-3.lua

--- Capstone/NodeMCU-Lua-Code/MailBox-using-Pushbutton/dht11-3.lua
+++ Capstone/NodeMCU-Lua-Code/MailBox-using-Pushbutton/dht11-3.lua
@@ -2,11 +2,7 @@ local module = {}
 gpio.mode(1,gpio.INT,gpio.PULLUP)
 
 local function tem()    
-        -- Integer firmware using this example
-           -- tmr.delay(3000000)
-            print("hello")
             m:publish("/sensors/temp","Mail Received",0,1)
-            print("hry")
 
 end
 
